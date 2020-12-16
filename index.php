195b45985b18e459c1a3bc54ca8a1ddc16c4a8a0
techamaan <74966563+techamaan@users.noreply.github.com>
2020-12-16 15:06:07 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,278 @@
+<?php
+
+$products = array(
+				"Electronics" => array(
+									"Television" => array(
+														array(
+															"id" => "PR001", 
+															"name" => "MAX-001",
+															"brand" => "Samsung"
+														),
+														array(
+															"id" => "PR002", 
+															"name" => "BIG-301",
+															"brand" => "Bravia"
+														),
+														array(
+															"id" => "PR003", 
+															"name" => "APL-02",
+															"brand" => "Apple"
+														)
+													),
+									"Mobile" => array(
+														array(
+															"id" => "PR004", 
+															"name" => "GT-1980",
+															"brand" => "Samsung"
+														),
+														array(
+															"id" => "PR005", 
+															"name" => "IG-5467",
+															"brand" => "Motorola"
+														),
+														array(
+															"id" => "PR006", 
+															"name" => "IP-8930",
+															"brand" => "Apple"
+														)
+													)
+								),
+				"Jewelry" => array(
+									"Earrings" => array(
+														array(
+															"id" => "PR007", 
+															"name" => "ER-001",
+															"brand" => "Chopard"
+														),
+														array(
+															"id" => "PR008", 
+															"name" => "ER-002",
+															"brand" => "Mikimoto"
+														),
+														array(
+															"id" => "PR009", 
+															"name" => "ER-003",
+															"brand" => "Bvlgari"
+														)
+													),
+									"Necklaces" => array(
+														array(
+															"id" => "PR010", 
+															"name" => "NK-001",
+															"brand" => "Piaget"
+														),
+														array(
+															"id" => "PR011", 
+															"name" => "NK-002",
+															"brand" => "Graff"
+														),
+														array(
+															"id" => "PR012", 
+															"name" => "NK-003",
+															"brand" => "Tiffany"
+														)
+													)				
+							)
+			)
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+	<section>
+		<h1 class="text-center">First Section</h1>
+		<table class="table" border="1">
+  <thead>
+    <tr>
+      <th scope="col">Category</th>
+      <th scope="col">Subcategory</th>
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">Brand</th>
+    </tr>
+  </thead>
+		
+<?php
+
+	foreach ($products as $key => $value) {
+				
+		foreach ($value as $key1 => $set1) {
+			
+			foreach ($set1 as $key2 => $set2) {
+				echo "<tr>";
+				echo "<td>".$key."</td>";
+				echo "<td>".$key1."</td>";
+				foreach ($set2 as $key3 => $set3) {
+					echo "<td>".$set3."</td>";
+				}
+				echo "</tr>";
+			}
+		}
+	}				
+
+?>
+		</table>
+
+</section>
+
+
+<section>
+	<h1 class="text-center">Second Section</h1>
+<table class="table" border="1">
+  <thead>
+    <tr>
+      <th scope="col">Category</th>
+      <th scope="col">Subcategory</th>
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">Brand</th>
+    </tr>
+  </thead>
+		
+<?php
+
+	foreach ($products as $key => $set) 
+		{	
+		foreach ($set as $key1 => $set1) 
+			{
+			foreach ($set1 as $key2 => $set2)
+			 {
+				if( $key1 == "Mobile" )
+				{
+				echo "<tr>";
+				echo "<td>".$key."</td>";
+				echo "<td>".$key3."</td>";
+				foreach ($set2 as $key3 => $set3)
+				 {
+					echo "<td>".$set3."</td>";
+				}
+				echo "</tr>";
+
+			}
+		}
+	}				
+}
+?>
+		</table>
+
+</section>
+
+<section>
+	<h1 class="text-center">Third Section</h1>
+	<table class="table" border="1">
+  <thead>
+    <tr>
+      <th scope="col">Category</th>
+      <th scope="col">Subcategory</th>	
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">Brand</th>
+    </tr>
+  </thead>
+
+  <?php
+
+foreach ($products as $key1=> $set) {
+	
+	foreach ($set as $key2 => $set1) {
+		
+		foreach ($set1 as $key3 => $set2) {
+			echo "<tr>";
+			foreach ($set2 as $key4 => $set3) {
+				if($set3=="Samsung"){
+					echo "<td>".$key1."</td>";
+				echo "<td>".$key2."</td>";
+				
+				foreach ($set2 as $key4 => $set3)
+				 {
+					echo "<td>".$set3."</td>";
+				}
+			}
+				echo "</tr>";
+			}
+		}
+	}	
+}	
+  ?>
+</table>
+</section>
+</section>
+	
+  <section>
+		<h1 class="text-center">Fourth Section</h1>	
+		<table class="table" border="1">
+  <thead>
+    <tr>
+      <th scope="col">Category</th>
+      <th scope="col">Subcategory</th>
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">Brand</th>
+    </tr>
+  </thead>
+		
+<?php
+	unset($products['Electronics']['Television'][2]);
+	foreach ($products as $key => $value) {
+				
+		foreach ($value as $key1 => $set1) {
+			
+			foreach ($set1 as $key2 => $set2) {
+				echo "<tr>";
+				echo "<td>".$key."</td>";
+				echo "<td>".$key1."</td>";
+				foreach ($set2 as $key3 => $set3) {
+					echo "<td>".$set3."</td>";
+				}
+				echo "</tr>";
+			}
+		}
+	}				
+
+?>
+		</table>
+
+</section>
+
+<section>
+	<h1 class="text-center">Fifth Section</h1>
+		<table class="table" border="1">
+  <thead>
+    <tr>
+      <th scope="col">Category</th>
+      <th scope="col">Subcategory</th>
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">Brand</th>
+    </tr>
+  </thead>
+		
+<?php
+   $products['Electronics']['Television'][1]['name']="BIG-555";
+	foreach ($products as $key => $value) {
+				
+		foreach ($value as $key1 => $set1) {
+			
+			foreach ($set1 as $key2 => $set2) {
+				echo "<tr>";
+				echo "<td>".$key."</td>";
+				echo "<td>".$key1."</td>";
+				foreach ($set2 as $key3 => $set3) {
+					echo "<td>".$set3."</td>";
+				}
+				echo "</tr>";
+			}
+		}
+	}				
+
+?>
+		</table>
+</section>
+</body>
+</html>
